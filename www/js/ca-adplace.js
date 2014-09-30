3a45def353a0611a5edeb9e39c4dc3a881c1e0e5
Edison A. Leon <eleon@mshanken.com>
2014-09-30 11:00:52 -0400

    harp site is reeady on mshanken

---
+++ www/js/ca-adplace.js
@@ -0,0 +1,79 @@
+// JavaScript Document
+
+<!--	
+function noimages(){
+	x = document.getElementById("menu1").style
+	y = document.getElementById("menu2").style
+	z = document.getElementById("menu3").style
+	
+	x.display = "block"
+	y.display = "none"
+	z.display = "none"
+}
+function sky(currMenu) {
+		x = document.getElementById("menu2").style
+		y = document.getElementById("menu3").style
+		
+		if(document.getElementById){
+		thisMenu = document.getElementById(currMenu).style
+		if(thisMenu.display =="block"){
+		thisMenu.display = "none"
+		x.display = "none"
+		y.display = "none"
+		}
+		else{
+			thisMenu.display = "block"
+			x.display = "none"
+			y.display = "none"
+			}
+			return false
+		}
+		else {
+			return true
+			}
+		}
+function leader(currMenu) {
+		x = document.getElementById("menu1").style
+		y = document.getElementById("menu3").style
+		
+		if(document.getElementById){
+		thisMenu = document.getElementById(currMenu).style
+		if(thisMenu.display =="block"){
+		thisMenu.display = "none"
+		x.display = "none"
+		y.display = "none"
+		}
+		else{
+			thisMenu.display = "block"
+			x.display = "none"
+			y.display = "none"
+			}
+			return false
+		}
+		else {
+			return true
+			}
+		}
+function road(currMenu) {
+		x = document.getElementById("menu1").style
+		y = document.getElementById("menu2").style
+		
+		if(document.getElementById){
+		thisMenu = document.getElementById(currMenu).style
+		if(thisMenu.display =="block"){
+		thisMenu.display = "none"
+		x.display = "none"
+		y.display = "none"
+		}
+		else{
+			thisMenu.display = "block"
+			x.display = "none"
+			y.display = "none"
+			}
+			return false
+		}
+		else {
+			return true
+			}
+		}
+-->
