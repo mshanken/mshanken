913fc4c84cef1793496c9450cb32f81114ce135d
Edison Leon <eleon@mshanken.com>
2018-05-10 16:40:15 -0400

    mshanken site version 2 is out

---
+++ web/js/app.js
@@ -0,0 +1,17 @@
+(function () {
+  "use strict";
+
+  // initialization of scroll animation
+  $.HSCore.components.HSOnScrollAnimation.init('[data-animation]');
+  
+})($);
+
+  
+$(window).on('load', function () {
+  // initialization of header
+  $.HSCore.components.HSHeader.init($('#js-header'));
+  $.HSCore.helpers.HSHamburgers.init('.hamburger');
+  // initialization of popups
+  $.HSCore.components.HSPopup.init('.js-fancybox');
+  $('.loading').fadeOut().remove();
+});
